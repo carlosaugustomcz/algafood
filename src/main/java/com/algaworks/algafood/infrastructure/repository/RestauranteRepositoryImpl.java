6e5c87443b6c5275fb9d03e9e442683bb6e1c392
carlosaugustomcz <carlos.augustomcz@gmail.com>
2024-04-10 17:15:05 -0300

    Inclusao do Restaurante

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -5,33 +5,40 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
-public class RestauranteRepositoryImpl  implements RestauranteRepository{
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
 
 	@PersistenceContext
 	private EntityManager manager;
 	
-	
-	@Override
-	public Restaurante buscar(Long id) {
-		return manager.find(Restaurante.class, id);	}
-	
 	@Override
 	public List<Restaurante> listar() {
 		return manager.createQuery("from Restaurante", Restaurante.class)
-				.getResultList();	}
-	
-	@Override
-	public void remover(Restaurante restaurante) {
-		restaurante = buscar(restaurante.getId());
-		manager.remove(restaurante);
-		
+				.getResultList();
 	}
 	
+	@Override
+	public Restaurante buscar(Long id) {
+		return manager.find(Restaurante.class, id);
+	}
+	
+	@Transactional
 	@Override
 	public Restaurante salvar(Restaurante restaurante) {
 		return manager.merge(restaurante);
 	}
+	
+	@Transactional
+	@Override
+	public void remover(Restaurante restaurante) {
+		restaurante = buscar(restaurante.getId());
+		manager.remove(restaurante);
+	}
+
 }
